88e81aa63e4a83e96235bbbb5b40d1617c36c28e
shgeta <shigeta@kanamei.co.jp>
2009-08-25 01:31:49 -0700

    ENV['EDITOR']

--- mysql/sample_create.rb
+++ mysql/sample_create.rb
@@ -1,14 +1,23 @@
-
 require 'rubygems'
 require "mysql"
 require 'pit'
 
+
+beforpitenv = ENV['EDITOR']
+ENV['EDITOR']="vi"
+afterpitseteditor = ENV['EDITOR']
 config = Pit.get("mysql_test02", :require => {
 	"db_host" => "db_host",
 	"db_user" => "db_user",
 	"db_pass" => "db_pass",
 	"db_name" => "db_name"
 })
+if afterpitseteditor == ENV['EDITOR'] then
+  ENV['EDITOR'] = beforpitenv
+else
+  raise "ENV['EDITOR'] not equal afterpitseteditor.  beforpitenv = #{beforpitenv}"
+end
+
 
 my = Mysql.init()
 my.options(Mysql::SET_CHARSET_NAME, "utf8")
